1650f843cd4bd85545dede6572968a845d8aad37
Serop Babikian <serop56@gmail.com>
2015-10-07 09:45:21 +0400

    Initial Commit

---
+++ TestGiTHomework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGiTHomework
+//
+//  Created by Serop Babikian on 10/7/15.
+//  Copyright © 2015 serop56.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
